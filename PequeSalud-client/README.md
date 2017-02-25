8c9d374118e7a3915a7eb03551dc16c5680a480b
Christian Rosado <emanuel_61199@yahoo.com>
2017-02-25 19:56:36 -0400

    jl

--- PequeSalud-client/README.md
+++
@@ -1,2 +0,0 @@
-# PequesaludApp
-Pequesalud 
---
+++ master.js
@@ -0,0 +1,19 @@
+var addButton = document.getElementById('addBtn');
+var articleForm = document.getElementById('artForm');
+var span = document.getElementsByClassName("close")[0];
+
+addButton.onclick = function() {
+    articleForm.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    articleForm.style.display = "none" 
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == articleForm) {
+        articleForm.style.display = "none";
+    }
+}
