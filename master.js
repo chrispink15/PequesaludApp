0b5a8ce2a8f485b0eff9fbde61b3263800ed40ae
Christian Rosado <emanuel_61199@yahoo.com>
2017-02-27 14:47:02 -0400

    and

--- master.js
+++ master.js
@@ -4,13 +4,20 @@ var span = document.getElementsByClassName("close")[0];
 var publishButton = document.getElementById("publicar");
 var display = document.getElementById('user_input');
 var title = document.getElementById('titulo').value;
+var arr = [];
 
-
-var articles = {
-  title: "Dengue en Puerto Rico",
-  message: "El Dengue en Puerto Rico cada dia crece mas, aprende como evitar contagearte.",
-  articulo: "lasjflkasjd;flajsdfjsad afaskldk sa  fs sdf s afs d s d fs  sa ds sad s fsd  s",
-};
+var articles = [
+  {
+    title: "Dengue en Puerto Rico",
+    message: "El Dengue en Puerto Rico cada dia crece mas, aprende como evitar contagearte.",
+    articulo: "lasjflkasjd;flajsdfjsad afaskldk sa  fs sdf s afs d s d fs  sa ds sad s fsd  s",
+  },
+  {
+    title: "La UPI hace huelga",
+    message: "Los estudiantes de la UPR hacen huelga en el capitolio.",
+    articulo: "lasjflkasjd;flajsdfjsad afaskldk sa  fs sdf s afs d s d fs  sa ds sad s fsd  s",
+  },
+];
 
 articleForm.style.display = "none";
 
@@ -35,9 +42,18 @@ function showMessage(){
             var article = document.getElementById('informacion').value;
             display_title.innerHTML= title;
             display_article.innerHTML = article;
+            articleForm.style.display = "none";
         }
+//function
+//
 
-function objArticle(){
-  articles['title'] = title;
-  articles['message'] = article;
+
+function objArticle(i){
+
+    display_title.innerHTML = articles[i].title;
+    display_article.innerHTML = articles[i].message;
+
 }
+
+objArticle(0);
+objArticle(1);
